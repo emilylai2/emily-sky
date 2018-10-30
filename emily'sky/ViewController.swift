444be49be7485988719a1eb888477f59f216cd3e
Emily lai <emilylai@macbook-pro.local>
2018-10-30 21:54:07 +0800

    Initial Commit

---
+++ emily'sky/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  emily'sky
+//
+//  Created by Emily lai on 2018/10/30.
+//  Copyright © 2018 Emily lai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
